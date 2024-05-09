e985ce45b4c93162d6f36e29a00046a7c4a425ee
Shivam Panicker <shivam.panicker@databricks.com>
2024-05-10 01:44:02 +1000

    first commit

--- dlt-wikipedia-python.py
+++ dlt-wikipedia-python.py
@@ -2,8 +2,10 @@
 import dlt
 from pyspark.sql.functions import *
 
-
+# COMMAND ----------
 json_path = "/databricks-datasets/wikipedia-datasets/data-001/clickstream/raw-uncompressed-json/2015_2_clickstream.json"
+
+# COMMAND ----------
 
 
 @dlt.table(
@@ -12,3 +14,37 @@ json_path = "/databricks-datasets/wikipedia-datasets/data-001/clickstream/raw-un
 def clickstream_raw():
     return (spark.read.format("json").load(json_path))
 
+# COMMAND ----------
+
+
+@dlt.table(
+    comment="Wikipedia clickstream data cleaned and prepared for analysis."
+)
+@dlt.expect("valid_current_page_title", "current_page_title IS NOT NULL")
+@dlt.expect_or_fail("valid_count", "click_count > 0")
+def clickstream_prepared():
+
+
+return (
+    dlt.read("clickstream_raw")
+    .withColumn("click_count", expr("CAST(n AS INT)"))
+    .withColumnRenamed("curr_title", "current_page_title")
+    .withColumnRenamed("prev_title", "previous_page_title")
+    .select("current_page_title", "click_count", "previous_page_title")
+)
+
+# COMMAND ----------
+
+
+@dlt.table(
+    comment="A table containing the top pages linking to the Apache Spark page."
+)
+def top_spark_referrers():
+    return (
+        dlt.read("clickstream_prepared")
+        .filter(expr("current_page_title == 'Apache_Spark'"))
+        .withColumnRenamed("previous_page_title", "referrer")
+        .sort(desc("click_count"))
+        .select("referrer", "click_count")
+        .limit(10)
+    )
